795fcbebf4de6a30065c039f228295bfd3b11e99
Samantha Lee <samantha.kayla.lee@gmail.com>
2023-10-20 14:36:56 -0700

    First commit

---
+++ Averager copy.cpp
@@ -0,0 +1,13 @@
+#include "Averager.h"
+
+Averager::Averager() {
+    total_wait_ = 0;
+    students_serviced_ = 0;
+}
+
+void Averager::process_student(Student student, int now) {
+    assert(now >= student.arrival_time());
+    total_wait_ += now-student.arrival_time();
+    total_question_time_ += student.question_duration();
+    students_serviced_ ++;
+}
